763b1a677a01bf9c943ce3fd5d0ed9549edbfd75
Rohyu-jeong <95173047+Rohyu-jeong@users.noreply.github.com>
2024-05-28 23:45:31 +0900

    Add files via upload

---
+++ Ex_0527_member/src/main/java/VO/MemberVO.java
@@ -0,0 +1,43 @@
+package VO;
+
+public class MemberVO {
+	private int idx; // 회원번호
+	private String name; // 이름
+	private String id; // 아이디
+	private String pwd; // 패스워드
+	private String email; // 이메일
+	
+	
+	public int getIdx() {
+		return idx;
+	}
+	public void setIdx(int idx) {
+		this.idx = idx;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getId() {
+		return id;
+	}
+	public void setId(String id) {
+		this.id = id;
+	}
+	public String getPwd() {
+		return pwd;
+	}
+	public void setPwd(String pwd) {
+		this.pwd = pwd;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	
+	
+}
