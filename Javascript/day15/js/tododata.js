4c2c9e268837c8a3650767a219963af2ada786a4
Rohyu-jeong <95173047+Rohyu-jeong@users.noreply.github.com>
2024-06-03 23:48:34 +0900

    Delete Javascript/day15 directory

--- Javascript/day15/js/tododata.js
+++
@@ -1,4 +0,0 @@
-export const dataList = Array(30).fill({
-    dataListDate: 1,
-    dataListToDoLists: []
-}).map(({dataListDate, dataListToDoLists}, i) => ({ dataListDate: i + 1, dataListToDoLists}))
